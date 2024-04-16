017d4163738e57fb3434d26459ba070417bc4163
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-04-15 20:29:28 -0400

    add beforeRouteLeave to prevent users from leaving dirty forms

--- client/src/components/users/UsersList.vue
+++ client/src/components/users/UsersList.vue
@@ -1,4 +1,5 @@
 <template>
+  <button @click="saveChanges">Save Changes</button>
   <ul>
     <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
   </ul>
@@ -11,10 +12,31 @@ export default {
   components: {
     UserItem,
   },
+  data() {
+    return { changesSaved: false }
+  },
   inject: ['users'],
   beforeRouteEnter(to, from, next) {
     console.log('UserList\'s component\'s beforeRouteEnter');
     next();
+  },
+  umounted() {
+    console.log('unmounted');
+  },
+  beforeRouteLeave(to, from, next) {
+    // calls this before we leave the component
+    console.log('UsersList Cmp beforeRouteLeave');
+    console.log(to, from);
+    if (this.changesSaved) {
+      next();
+    } else {
+      next(confirm('Are you sure? You got unsaved changes!'));
+    }
+  },
+  methods: {
+    saveChanges() {
+      this.changesSaved = true;
+    }
   }
 };
 </script>
